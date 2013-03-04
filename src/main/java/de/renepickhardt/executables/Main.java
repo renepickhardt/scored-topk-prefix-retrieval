ea6a9fa1c14760862514786ddcdb594f1f0ab226
Rene Pickhardt <rene@rene-pickhardt.de>
2013-03-04 13:16:52 +0100

    created maven project

---
+++ src/main/java/de/renepickhardt/executables/Main.java
@@ -0,0 +1,14 @@
+package de.renepickhardt.executables;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("lets go!");
+
+	}
+
+}
